05bf8b21025f9730e133a9de97b7f40a8b4f58a5
Matthew Mettler <matthewrmettler@gmail.com>
2016-08-20 12:33:26 -0400

    Setting up routing tables, installed Connect.

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 /**
  * Created by Matt on 8/20/2016.
  */
-console.log("Hello world! Node is up and running.");
+
+/** Setup **/
+var connect = require('connect');
+var app = connect();
+var port = 3000;
+
+/** Routing **/
+var logger = function(req, res, next) {
+    console.log(req.method, req.url);
+
+    next();
+};
+
+var helloWorld = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world!');
+};
+
+var goodbyeWorld = function(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye world!');
+};
+
+/** Connect the routers **/
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
+/** Run the server **/
+app.listen(port);
+console.log("Server running at localhost:" + port);
