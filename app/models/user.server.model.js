bc351dc27cf65b5a6cbe6f9089a22d819c1e84e8
Matthew Mettler <matthewrmettler@gmail.com>
2016-08-26 23:33:21 -0400

    Introduced virtual attributes.

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -39,5 +39,16 @@ var UserSchema = new Schema({
     }
 });
 
-UserSchema.set('toJSON', { getters: true});
+/** Virtual attributes **/
+UserSchema.virtual('fullName')
+    .get(function() {
+        return this.firstName + " " + this.lastName;
+    }).set(function(fullName) {
+        var splitName = fullName.split(' ');
+        this.firstName = splitName[0] || "";
+        this.lastName = splitName[1] || "";
+});
+
+
+UserSchema.set('toJSON', { getters: true, virtuals: true});
 mongoose.model('User', UserSchema);
