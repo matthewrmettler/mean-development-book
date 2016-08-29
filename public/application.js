756089c7f8e3e45365da55d305f729934f5a3d15
Matthew Mettler <matthewrmettler@gmail.com>
2016-08-29 17:27:30 -0400

    Introduced basic angular views, and used ng-include to render it inside index.ejs.

--- public/application.js
+++ public/application.js
@@ -5,7 +5,7 @@
 /** Manual Angular Boostrapping **/
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, []);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
 
 angular.element(document).ready(function() {
    angular.bootstrap(document, [mainApplicationModuleName])
---
+++ public/example/example.client.module.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Matt on 8/29/2016.
+ */
+
+angular.module('example', []);
